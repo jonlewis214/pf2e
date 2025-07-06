156c9feb901705b305717e3efecd18b3fc85d19b
Jon Lewis <jonlewis214@gmail.com>
2025-07-06 15:43:55 -0500

    rename 🐵🩹.ts for windows file system

--- src/scripts/hooks/load.ts
+++ src/scripts/hooks/load.ts
@@ -45,7 +45,7 @@ import {
     TokenDocumentPF2e,
 } from "@scene/index.ts";
 import { ActorDeltaPF2e } from "@scene/token-document/actor-delta.ts";
-import { monkeyPatchFoundry } from "@scripts/🐵🩹.ts";
+import { monkeyPatchFoundry } from "@scripts/monkey-patch.ts";
 import { CheckRoll, StrikeAttackRoll } from "@system/check/roll.ts";
 import { ClientDatabaseBackendPF2e } from "@system/client-backend.ts";
 import { DamageInstance, DamageRoll } from "@system/damage/roll.ts";
